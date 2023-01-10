84ebedfbdbd84108789ca35a718c83e59f1150c0
Your Name <dfsilvat@gmail.com>
2023-01-09 22:14:49 -0300

    config express

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const host = 8081;
+
+//View engine
+app.set('view engine', 'ejs');
+
+// Static
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Funcionando')
+})
+
+
+app.listen(host, () => {
+    console.log(`Server ON: http://localhost:${host}`)
+})
